5330b3034bae50ce775c15da8c82778a46920213
Timoth37 <timothee.gallais@edu.devinci.fr>
2023-02-16 08:56:50 +0100

    Small bug resolution

--- locations/svelte.config.js
+++ locations/svelte.config.js
@@ -1,6 +1,9 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
+
 export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// see the 'Deployment configuration' section below
+		})
 	}
 };
